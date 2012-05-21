e04a826a55b49c6f73a3f5b1e003d8d17f451777
Paul Martin <pmartin@nmc.edu>
2012-05-21 15:55:04 -0400

    Removing the static "admin_presets" and adding submodule

--- blocks/admin_presets/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$plugin->version = 2012032800;
-$plugin->release = '1.0(Build: 2012032800)';
-
-$plugin->requires = 2010112400;
-$plugin->cron     = 0;
-$plugin->maturity = MATURITY_STABLE;
