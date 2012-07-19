b341d4b51a6e7e46ea15c351707aa19af9f79abd
Paul Martin <pmartin@nmc.edu>
2012-07-18 22:37:15 -0400

    Adding nanogong mod/filter/TinyMCE

--- lib/editor/tinymce/lib.php
+++ lib/editor/tinymce/lib.php
@@ -191,6 +191,8 @@ class tinymce_texteditor extends texteditor {
         if (!empty($options['required'])) {
             $params['init_instance_callback'] = 'M.editor_tinymce.onblur_event';
         }
+        $params['plugins'] .= ',nanogong';
+        $params['theme_advanced_buttons3'] .=',nanogong';
         return $params;
     }
 }
