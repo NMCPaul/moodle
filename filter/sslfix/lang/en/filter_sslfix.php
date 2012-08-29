ae79eb3a5910476b52740ed231a2924fdd5a5ed7
Paul Martin <pmartin@nmc.edu>
2012-08-29 13:38:01 -0400

    Adding the SSLfix filter

---
+++ filter/sslfix/lang/en/filter_sslfix.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Strings for filter_glossary
+ *
+ * @package    filter
+ * @subpackage glossary
+ * @copyright  2011 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['filtername'] = 'SSL / Voki Fix';
