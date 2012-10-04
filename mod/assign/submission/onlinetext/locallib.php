d8ebe09e0a6201373cb3546c76695954b029a4ce
Paul Martin <pmartin@nmc.edu>
2012-10-04 15:50:44 -0400

    Increased shorttext limit from 140 to 500 on "Submission Status" screen

--- mod/assign/submission/onlinetext/locallib.php
+++ mod/assign/submission/onlinetext/locallib.php
@@ -201,7 +201,7 @@ class assign_submission_onlinetext extends assign_submission_plugin {
 
         if ($onlinetextsubmission) {
             $text = format_text($onlinetextsubmission->onlinetext, $onlinetextsubmission->onlineformat, array('context'=>$this->assignment->get_context()));
-            $shorttext = shorten_text($text, 140);
+            $shorttext = shorten_text($text, 500);
             if ($text != $shorttext) {
                 return $shorttext . get_string('numwords', 'assignsubmission_onlinetext', count_words($text));
             } else {
